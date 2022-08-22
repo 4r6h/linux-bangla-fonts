1d74d771fde8cb826117d89e7847cf85cac6d6cb
4r6h <hrahat023@gmail.com>
2022-08-22 10:47:41 +0000

    Testing

--- createArchieve.sh
+++ createArchieve.sh
@@ -17,7 +17,6 @@ archieveBangla="LinuxBanglaFonts.tar.gz"
 archieveUrdu="LinuxUrduFonts.tar.gz"
 archieveArabic="LinuxArabicFonts.tar.gz"
 
-
 #if [ -e "$archieveFolder/$archiveBangla" ]; then
 #    echo "File exists";
 #    rm $archieveFolder/$archiveBangla;
@@ -27,13 +26,13 @@ archieveArabic="LinuxArabicFonts.tar.gz"
 #    cd $BanglaFolder; tar -zcvf LinuxBanglaFonts.tar.gz *;mv $archieveBangla ../$archieveFolder/;
 #fi 
 
-if [ -e "$archieveFolder/$archiveUrdu" ]; then
-    echo "File exists";
-    rm "$archieveFolder/$archiveUrdu"
-    cd $UrduFolder; tar -zcvf LinuxUrduFonts.tar.gz *;mv $archieveUrdu ../$archieveFolder/;
-else
-    echo "File does not exist";
-    cd $UrduFolder; tar -zcvf LinuxUrduFonts.tar.gz *;mv $archieveUrdu ../$archieveFolder/;
+if [ -e $archieveFolder/$archiveUrdu ]; then
+    echo File exists
+    rm archieve/LinuxUrduFonts.tar.gz
+    cd $UrduFolder; tar -zcvf LinuxUrduFonts.tar.gz *;mv $archieveUrdu ../$archieveFolder/
+elif [ ! -e $archieveFolder/$archiveUrdu ]; then
+    echo File does not exist
+    cd $UrduFolder; tar -zcvf LinuxUrduFonts.tar.gz *;mv $archieveUrdu ../$archieveFolder/
 fi 
 
 #if [ -e "$archieveFolder/$archiveArabic" ]; then
