112bc0a6cd0a83a39371c51157ee177885d03f9a
4r6h <hrahat023@gmail.com>
2022-08-22 10:15:30 +0000

    Added Urdu Fonts

--- createArchieve.sh
+++ createArchieve.sh
@@ -10,15 +10,38 @@
 
 
 archieveFolder="archieve"
-fontsFolder="fonts"
-archieveName="LinuxBanglaFonts.tar.gz"
+BanglaFolder="Bangla-Fonts"
+UrduFolder="Urdu-Fonts"
+ArabicFolder="Arabic-Fonts"
+archieveBangla="LinuxBanglaFonts.tar.gz"
+archieveUrdu="LinuxUrduFonts.tar.gz"
+archieveArabic="LinuxArabicFonts.tar.gz"
 
 
-if [ -e "$archieveFolder/$archieveName" ]; then
+#if [ -e "$archieveFolder/$archiveBangla" ]; then
+#    echo "File exists";
+#    rm $archieveFolder/$archiveBangla;
+#    cd $BanglaFolder; tar -zcvf LinuxBanglaFonts.tar.gz *;mv $archieveBangla ../$archieveFolder/;
+#else
+#    echo "File does not exist";
+#    cd $BanglaFolder; tar -zcvf LinuxBanglaFonts.tar.gz *;mv $archieveBangla ../$archieveFolder/;
+#fi 
+
+if [ -e "$archieveFolder/$archiveUrdu" ]; then
     echo "File exists";
-    rm $archieveFolder/$archieveName;
-    cd $fontsFolder; tar -zcvf LinuxBanglaFonts.tar.gz *;mv $archieveName ../$archieveFolder/;
+    rm "$archieveFolder/$archiveUrdu"
+    cd $UrduFolder; tar -zcvf LinuxUrduFonts.tar.gz *;mv $archieveUrdu ../$archieveFolder/;
 else
     echo "File does not exist";
-    cd $fontsFolder; tar -zcvf LinuxBanglaFonts.tar.gz *;mv $archieveName ../$archieveFolder/;
+    cd $UrduFolder; tar -zcvf LinuxUrduFonts.tar.gz *;mv $archieveUrdu ../$archieveFolder/;
 fi 
+
+#if [ -e "$archieveFolder/$archiveArabic" ]; then
+#    echo "File exists";
+#    rm $archieveFolder/$archiveArabic;
+#    cd $ArabicFolder; tar -zcvf LinuxArabicFonts.tar.gz *;mv $archieveArabic ../$archieveFolder/;
+#else
+#    echo "File does not exist";
+#    cd $ArabicFolder; tar -zcvf LinuxArabicFonts.tar.gz *;mv $archieveArabic ../$archieveFolder/;
+#fi 
+#
